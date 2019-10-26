dd7adc0b146e711ca0758e7ebc0829fa72531b60
rachel doran <rmitchell0925@gmail.com>
2019-10-26 15:27:29 -0400

    we got an error

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Blackbean Burger');
+INSERT INTO burgers (name) VALUES ("Bobbi's Burger");
+INSERT INTO burgers (name, devoured) VALUES ('Big Mac', true);
+INSERT INTO burgers (name, devoured) VALUES ('Whopper', true);
+INSERT INTO burgers (name, devoured) VALUES ('Good Burger', true);
+
