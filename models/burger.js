0caba5a3e20e8b342da037a4d57ee5a49a173fd8
rachel doran <rmitchell0925@gmail.com>
2019-10-26 16:41:40 -0400

    idk

--- models/burger.js
+++ models/burger.js
@@ -1,28 +1,28 @@
-// Import the ORM to create functions that will interact with the database.
 var orm = require("../config/orm.js");
 
 var burger = {
-  all: function(cb) {
-    orm.all("burgers", function(res) {
+  all: function (cb) {
+    orm.allOrder("burgers", "date, id", function (res) {
       cb(res);
     });
   },
-  // The variables cols and vals are arrays.
-  create: function(cols, vals, cb) {
-    orm.create("burgers", cols, vals, function(res) {
+  create: function (vals, cb) {
+    orm.create("burgers", ['burger_name'], vals, function (res) {
       cb(res);
     });
   },
-  update: function(objColVals, condition, cb) {
-    orm.update("burgers", objColVals, condition, function(res) {
+  update: function (objColVals, condition, cb) {
+    objColVals.date = new Date().toISOString().slice(0, 19).replace('T', ' ');
+    orm.update("burgers", objColVals, condition, function (res) {
       cb(res);
     });
   },
-  delete: function(condition, cb) {
-    orm.delete("burgers", condition, function(res) {
+  delete: function (condition, cb) {
+    orm.delete("burgers", condition, function (res) {
       cb(res);
     });
   }
 };
 
+// Export the database functions for the controller (catsController.js).
 module.exports = burger;
