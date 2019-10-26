953b608e15f0f85c25a050a8a1aaa4228bfd5416
rachel doran <rmitchell0925@gmail.com>
2019-10-26 09:49:27 -0400

    working on it

--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,10 @@
-### Schema
-
 CREATE DATABASE burgers_db;
 USE burgers_db;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
-	name varchar(255) NOT NULL,
-	sleepy BOOLEAN DEFAULT false,
+	burger_name varchar(255) NOT NULL,
+	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
